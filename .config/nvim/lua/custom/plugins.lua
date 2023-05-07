285380a3829e77065249e2d7fe215d985a5ba06c
Jay <leafjay001@gmail.com>
2023-05-07 16:27:50 +0200

    autosave

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -29,6 +29,23 @@ local plugins = {
       }
     }
   },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({})
+    end
+  },
+
+  {
+    "Pocco81/auto-save.nvim",
+    event = "VeryLazy",
+    config = function ()
+      require("auto-save").setup({})
+    end
+  },
 }
 
 return plugins
